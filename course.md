c7fb93ef5a48f11078d4fe84e2c9d3b216b08ae1
Mario Conde <mcodegt@gmail.com>
2023-03-29 14:18:59 -0600

    updates

---
+++ course.md
@@ -0,0 +1,6 @@
+### `React`
+
+### `Clasess vs Hooks`
+
+1. just part of Javascript. 
+2. 
--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react'
-import { robots } from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import './App.css';
 
 class App extends Component{
+
+    
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield:''
         }
+    }
+
+    componentDidMount(){
+        
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users =>{
+            this.setState({robots: users})
+        })
+        
     }
 
     onSearchChange = (event) =>{
